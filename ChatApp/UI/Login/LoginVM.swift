d4ea6cdd93c1086e6b71b5a048672c593cc6eeb8
Antonio Ivcec <antonio.ivcec@protonmail.com>
2018-10-05 12:47:50 +0200

    LoginVM - added validators to fields

--- ChatApp/UI/Login/LoginVM.swift
+++ ChatApp/UI/Login/LoginVM.swift
@@ -13,18 +13,43 @@ class LoginVM: LoginVMType {
     weak var coordinatorDelegate: LoginVMCoordinatorDelegate?
     private let service: LoginService
     
+    // MARK: - Validators
+    
+    private let nameValidator: StringValidator
+    private let emailValidator: StringValidator
+    private let passValidator: StringValidator
+
     // MARK: - Init
     
     init(delegate: LoginVMCoordinatorDelegate, service: LoginService) {
         self.coordinatorDelegate = delegate
         self.service = service
+        
+        self.nameValidator = LengthValidator(minLength: 3)
+        self.emailValidator = EmailValidator()
+        self.passValidator = LengthValidator(minLength: 6)
     }
     
     // MARK: - Actions
     
     func register(email: String?, password: String?, name: String?, image: UIImage?) {
         guard let email = email, let password = password, let name = name, let image = image else {
-            print("Form is not valid")
+            showRegisterInfoMissingError()
+            return
+        }
+        
+        if !nameValidator.isValid(name) {
+            showNameValidationError()
+            return
+        }
+        
+        if !emailValidator.isValid(email) {
+            showEmailValidationError()
+            return
+        }
+        
+        if !passValidator.isValid(password) {
+            showPassValidationError()
             return
         }
         
@@ -38,12 +63,44 @@ class LoginVM: LoginVMType {
     
     func login(email: String?, password: String?) {
         guard let email = email, let password = password else {
-            print("Form is not valid")
+            showLoginInfoMissingError()
+            return
+        }
+        
+        if !emailValidator.isValid(email) {
+            showEmailValidationError()
+            return
+        }
+        
+        if !passValidator.isValid(password) {
+            showPassValidationError()
             return
         }
         
         service.login(email: email, password: password) {
             self.coordinatorDelegate?.navigate(to: .login)
         }
+    }
+    
+    // MARK: - Alerts
+    
+    private func showLoginInfoMissingError() {
+        coordinatorDelegate?.showAlert(title: "Error", message: "Please enter email and pasword")
+    }
+    
+    private func showRegisterInfoMissingError() {
+        coordinatorDelegate?.showAlert(title: "Error", message: "Please add name, email, pasword and profile image")
+    }
+    
+    private func showNameValidationError() {
+        coordinatorDelegate?.showAlert(title: "Error", message: "Name should be at least 3 characters long")
+    }
+    
+    private func showEmailValidationError() {
+        coordinatorDelegate?.showAlert(title: "Error", message: "Please enter a valid email")
+    }
+    
+    private func showPassValidationError() {
+        coordinatorDelegate?.showAlert(title: "Error", message: "Password should be at least 6 characters long")
     }
 }
